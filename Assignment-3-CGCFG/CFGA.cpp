0a1502662e56061ba577a833c6b963c5688fcbfd
chenlu20 <chen181396@163.com>
2025-11-15 14:47:14 +0800

    Update CFGA.cpp

--- Assignment-3-CGCFG/CFGA.cpp
+++ Assignment-3-CGCFG/CFGA.cpp
@@ -49,6 +49,79 @@ int main(int argc, char **argv)
 //         }
 // }
 
+// void CFGAnalysis::analyze(SVF::ICFG *icfg)
+// {
+//     std::function<void(unsigned, unsigned, std::vector<unsigned>&, std::set<unsigned>&)> dfs;
+    
+//     dfs = [&](unsigned current, unsigned sink, 
+//               std::vector<unsigned> &path, std::set<unsigned> &visited) -> void
+//     {
+//         path.push_back(current);
+//         visited.insert(current);
+        
+//         if (current == sink)
+//         {
+//             recordPath(path);
+//             path.pop_back();
+//             visited.erase(current);
+//             return;
+//         }
+        
+//         auto node = icfg->getICFGNode(current);
+        
+//         if (auto callNode = llvm::dyn_cast<SVF::CallICFGNode>(node))
+//         {
+//             callStack.push(current);
+            
+//             for (auto edge : node->getOutEdges())
+//             {
+//                 unsigned succ = edge->getDstID();
+//                 if (visited.find(succ) == visited.end())
+//                 {
+//                     dfs(succ, sink, path, visited);
+//                 }
+//             }
+            
+//             if (!callStack.empty())
+//                 callStack.pop();
+//         }
+//         else if (auto retNode = llvm::dyn_cast<SVF::RetICFGNode>(node))
+//         {
+//             for (auto edge : node->getOutEdges())
+//             {
+//                 unsigned succ = edge->getDstID();
+//                 if (visited.find(succ) == visited.end())
+//                 {
+//                     dfs(succ, sink, path, visited);
+//                 }
+//             }
+//         }
+//         else
+//         {
+//             for (auto edge : node->getOutEdges())
+//             {
+//                 unsigned succ = edge->getDstID();
+//                 if (visited.find(succ) == visited.end())
+//                 {
+//                     dfs(succ, sink, path, visited);
+//                 }
+//             }
+//         }
+        
+//         path.pop_back();
+//         visited.erase(current);
+//     };
+    
+//     for (auto src : sources)
+//     {
+//         for (auto snk : sinks)
+//         {
+//             std::vector<unsigned> path;
+//             std::set<unsigned> visited;
+//             dfs(src, snk, path, visited);
+//         }
+//     }
+// }
 void CFGAnalysis::analyze(SVF::ICFG *icfg)
 {
     std::function<void(unsigned, unsigned, std::vector<unsigned>&, std::set<unsigned>&)> dfs;
@@ -59,6 +132,7 @@ void CFGAnalysis::analyze(SVF::ICFG *icfg)
         path.push_back(current);
         visited.insert(current);
         
+        // 到达sink，记录路径
         if (current == sink)
         {
             recordPath(path);
@@ -69,10 +143,13 @@ void CFGAnalysis::analyze(SVF::ICFG *icfg)
         
         auto node = icfg->getICFGNode(current);
         
+        // 处理调用节点
         if (auto callNode = llvm::dyn_cast<SVF::CallICFGNode>(node))
         {
+            // 将调用点压栈
             callStack.push(current);
             
+            // 遍历所有出边
             for (auto edge : node->getOutEdges())
             {
                 unsigned succ = edge->getDstID();
@@ -82,20 +159,29 @@ void CFGAnalysis::analyze(SVF::ICFG *icfg)
                 }
             }
             
+            // 探索完成后弹栈
             if (!callStack.empty())
                 callStack.pop();
         }
+        // 处理返回节点
         else if (auto retNode = llvm::dyn_cast<SVF::RetICFGNode>(node))
         {
+            // 返回节点只应该返回到对应的调用点
             for (auto edge : node->getOutEdges())
             {
                 unsigned succ = edge->getDstID();
-                if (visited.find(succ) == visited.end())
+                
+                // 检查调用栈是否匹配
+                if (!callStack.empty())
                 {
-                    dfs(succ, sink, path, visited);
+                    if (visited.find(succ) == visited.end())
+                    {
+                        dfs(succ, sink, path, visited);
+                    }
                 }
             }
         }
+        // 普通节点
         else
         {
             for (auto edge : node->getOutEdges())
@@ -112,12 +198,14 @@ void CFGAnalysis::analyze(SVF::ICFG *icfg)
         visited.erase(current);
     };
     
+    // 对每对source-sink执行DFS
     for (auto src : sources)
     {
         for (auto snk : sinks)
         {
             std::vector<unsigned> path;
             std::set<unsigned> visited;
+            callStack = std::stack<unsigned>(); // 清空调用栈
             dfs(src, snk, path, visited);
         }
     }
